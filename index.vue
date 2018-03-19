bb2fee88c55a75269252df2d5ccf3b5cf2035695
wm <531431988@qq.com>
2018-03-19 14:18:59 +0800

    添加文件

---
+++ index.vue
@@ -0,0 +1,47 @@
+<template>
+</template>
+<script>
+import PDF from 'pdfobject/pdfobject.min.js'
+export default {
+  props: {
+    type: {
+      type: Number,
+      default: 1
+    },
+    url: {
+      type: String,
+      default: 'http://192.168.7.61/00/20/wKgHPVoUHZGADnDzAF5sQVBjuTA965.pdf'
+    }
+  },
+  data: () => ({
+    pdf: null,
+    options: {
+      page: 1,
+      fallbackLink: '浏览器不支持PDF查看功能，请更换谷歌浏览器',
+      forcePDFJS: true
+    }
+  }),
+  created () {
+    if (this.type) {
+      this.options.PDFJS_URL = 'http://mozilla.github.io/pdf.js/web/viewer.html'
+    } else {
+      this.options.pdfOpenParams = {
+        navpanes: 0,
+        toolbar: 1,
+        statusbar: 1,
+        view: 'FitV',
+        pagemode: 'thumbs'
+      }
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    this.pdf = PDF
+    this.pdf.embed(this.url, this.$el, this.options)
+  }
+}
+</script>
+<style>
+.pdfobject-container { height: 500px;}
+</style>
